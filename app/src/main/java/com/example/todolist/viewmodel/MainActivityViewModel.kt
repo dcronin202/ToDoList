edbaf35c75d4ddc2e807e1fc570becfe2d4c6ff5
dcronin202 <dcronin202@gmail.com>
2021-04-19 12:57:46 -0400

    Created view model

---
+++ app/src/main/java/com/example/todolist/viewmodel/MainActivityViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.todolist.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.todolist.model.MockTaskList
+import com.example.todolist.model.Task
+
+class MainActivityViewModel : ViewModel() {
+
+    companion object {
+        private val TAG = MainActivityViewModel::class.java.simpleName
+    }
+    
+    // GET LIST OF TASKS
+    private val taskListData = MutableLiveData<List<Task>>()
+    val fetchTasks: LiveData<List<Task>>
+        get() = taskListData
+
+    private val newTaskList = arrayListOf<Task>()
+    private val mockTasks = MockTaskList.getMockTaskList()
+
+    private fun updateLiveData(list: List<Task>) {
+        taskListData.value = list
+    }
+
+    fun fetchMockTasks() {
+        newTaskList.addAll(mockTasks)
+        updateLiveData(newTaskList)
+    }
+
+}
