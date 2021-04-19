a1d4887bac2c1273940e8024faed65aba5939acc
dcronin202 <dcronin202@gmail.com>
2021-04-19 11:52:18 -0400

    Created mock task list object

---
+++ app/src/main/java/com/example/todolist/model/MockTaskList.kt
@@ -0,0 +1,23 @@
+package com.example.todolist.model
+
+object MockTaskList {
+    
+    fun getMockTaskList(): List<Task> {
+        return listOf(
+            Task(state = 0, content = "Task 1"),
+            Task(state = 0, content = "Task 2"),
+            Task(state = 0, content = "Task 3"),
+            Task(state = 0, content = "Task 4"),
+            Task(state = 0, content = "Task 5"),
+            Task(state = 0, content = "Task 6"),
+            Task(state = 0, content = "Task 7"),
+            Task(state = 0, content = "Task 8"),
+            Task(state = 0, content = "Task 9"),
+            Task(state = 0, content = "Task 10"),
+            Task(state = 0, content = "Task 11"),
+            Task(state = 0, content = "Task 12"),
+            Task(state = 0, content = "Task 13"),
+            Task(state = 0, content = "Task 14")
+        )
+    }
+}
