9f04fc562272938ce28ebb1097ead42cfd723ce3
dcronin202 <dcronin202@gmail.com>
2021-04-19 13:16:40 -0400

    Added the viewmodel and recycler view set up to the main activity's onCreate

--- app/src/main/java/com/example/todolist/view/MainActivity.kt
+++ app/src/main/java/com/example/todolist/view/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.todolist.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.todolist.R
 import com.example.todolist.databinding.ActivityMainBinding
+import com.example.todolist.viewmodel.MainActivityViewModel
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +15,9 @@ class MainActivity : AppCompatActivity() {
         private val TAG = MainActivity::class.java.simpleName
     }
 
+    private lateinit var mainActivityViewModel: MainActivityViewModel
+    private lateinit var linearLayoutManager: LinearLayoutManager
+    private lateinit var recyclerAdapter: TaskListRecyclerAdapter
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,10 +25,41 @@ class MainActivity : AppCompatActivity() {
 
         // DATA BINDING
         setupDataBinding()
+        // VIEW MODEL
+        setupViewModel()
+        // RECYCLER VIEW OF TASKS
+        setupTaskList()
+        //OBSERVER
+        observeTaskData()
+
     }
 
-    // SETUP DATA BINDING
+    // SET UP DATA BINDING
     private fun setupDataBinding() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
+
+    // SET UP VIEWMODEL
+    private fun setupViewModel() {
+        mainActivityViewModel = ViewModelProviders.of(this).get(MainActivityViewModel::class.java)
+    }
+
+    // SET UP TASK LIST IN RECYCLER VIEW
+    private fun setupTaskList() {
+        linearLayoutManager = LinearLayoutManager(this)
+        recyclerAdapter = TaskListRecyclerAdapter(taskList = arrayListOf())
+        binding.recyclerView.adapter = recyclerAdapter
+        binding.recyclerView.layoutManager = linearLayoutManager
+    }
+
+    // OBSERVE DATA
+    private fun observeTaskData() {
+        mainActivityViewModel.fetchMockTasks()
+
+        mainActivityViewModel.fetchTasks.observe(this) { tasks ->
+            recyclerAdapter.updateTasks(tasks)
+        }
+
+    }
+
 }
