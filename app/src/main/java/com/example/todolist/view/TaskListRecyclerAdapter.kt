6a74ac5175c9508805a2094f3b62ce0f59fd7f90
dcronin202 <dcronin202@gmail.com>
2021-04-19 12:44:03 -0400

    Created recycler adapter and task item layout

---
+++ app/src/main/java/com/example/todolist/view/TaskListRecyclerAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.todolist.view
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todolist.databinding.TaskItemLayoutBinding
+import com.example.todolist.model.Task
+
+class TaskListRecyclerAdapter(private var taskList: List<Task> = listOf())
+    : RecyclerView.Adapter<TaskListRecyclerAdapter.TaskListViewHolder>() {
+
+    inner class TaskListViewHolder(val binding: TaskItemLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(task: Task) {
+            binding.taskContent.text = task.content
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskListViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = TaskItemLayoutBinding.inflate(inflater, parent, false)
+        return TaskListViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: TaskListViewHolder, position: Int) =
+        holder.bind(taskList[position])
+
+    override fun getItemCount(): Int = taskList.size
+
+}
