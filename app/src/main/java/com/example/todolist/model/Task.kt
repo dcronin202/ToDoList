f852d56d75ce70b1e871886ed3449b8304f1d5d8
dcronin202 <dcronin202@gmail.com>
2021-04-20 10:28:32 -0400

    Added functions to track the task state changes in the view model

--- app/src/main/java/com/example/todolist/model/Task.kt
+++ app/src/main/java/com/example/todolist/model/Task.kt
@@ -1,3 +1,26 @@
 package com.example.todolist.model
 
-data class Task(private var state: Int, val content: String)
+data class Task(private var state: Int, val content: String) {
+
+    fun getStateAsEnum(): TaskState? {
+        return when (state) {
+            TaskState.CURRENT.ordinal -> TaskState.CURRENT
+            TaskState.COMPLETED.ordinal -> TaskState.COMPLETED
+            TaskState.DELETED.ordinal -> TaskState.DELETED
+            else -> null
+        }
+    }
+    
+    fun complete() {
+        state = TaskState.COMPLETED.ordinal
+    }
+
+    fun resetToCurrent() {
+        state = TaskState.CURRENT.ordinal
+    }
+
+    fun delete() {
+        state = TaskState.DELETED.ordinal
+    }
+
+}
--- app/src/main/java/com/example/todolist/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/com/example/todolist/viewmodel/MainActivityViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.todolist.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -17,6 +18,11 @@ class MainActivityViewModel : ViewModel() {
     val fetchTasks: LiveData<List<Task>>
         get() = taskListData
 
+    // TRACK TASK STATE CHANGE WHEN USER ADDS, COMPLETES, OR DELETES A TASK
+    private val taskStateChange = MutableLiveData<Task>()
+    val fetchTaskState: LiveData<Task>
+        get() = taskStateChange
+
     private val newTaskList = arrayListOf<Task>()
     private val mockTasks = MockTaskList.getMockTaskList()
 
@@ -31,11 +37,57 @@ class MainActivityViewModel : ViewModel() {
 
     fun addTask(task: Task) {
         newTaskList.add(task)
+        taskStateChange.postValue(task)
+        updateLiveData(newTaskList)
+    }
+
+    fun completeTask(task: Task) {
+        Log.d(TAG, "Original state is $task")
+
+        // Search for task in list
+        val taskInList = newTaskList.firstOrNull {
+            it == task
+        } ?: return
+
+        // Update state to COMPLETE
+        taskInList.complete()
+
+        // Remove from current position in list...
+        newTaskList.remove(taskInList)
+
+        // ...and add to end of the list
+        newTaskList.add(newTaskList.size, taskInList)
+
+        taskStateChange.postValue(taskInList)
+        updateLiveData(newTaskList)
+
+        Log.d(TAG, "State updated to $task")
+    }
+
+    fun resetToCurrent(task: Task) {
+        val taskInList = newTaskList.firstOrNull {
+            it == task
+        } ?: return
+
+        taskInList.resetToCurrent()
+        // Move to top of the list
+        newTaskList.remove(taskInList)
+        newTaskList.add(0, taskInList)
+
+        taskStateChange.postValue(taskInList)
         updateLiveData(newTaskList)
     }
 
     fun deleteTask(task: Task) {
-        newTaskList.remove(task)
+        val taskInList = newTaskList.firstOrNull {
+            it == task
+        } ?: return
+
+        taskInList.delete()
+
+        taskStateChange.postValue(taskInList)
+        newTaskList.remove(taskInList)
+
         updateLiveData(newTaskList)
     }
 
