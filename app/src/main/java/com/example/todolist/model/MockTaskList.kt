34c37af4e4ef98685534266a5ce27fc28976a5e3
dcronin202 <dcronin202@gmail.com>
2021-04-22 14:53:04 -0400

    Created two mock lists for current and completed tasks

--- app/src/main/java/com/example/todolist/model/MockTaskList.kt
+++ app/src/main/java/com/example/todolist/model/MockTaskList.kt
@@ -2,22 +2,28 @@ package com.example.todolist.model
 
 object MockTaskList {
 
-    fun getMockTaskList(): List<Task> {
+    fun getCurrentTaskList(): List<DataListItem> {
         return listOf(
-            Task(state = 0, content = "Task 1"),
-            Task(state = 0, content = "Task 2"),
-            Task(state = 0, content = "Task 3"),
-            Task(state = 0, content = "Task 4"),
-            Task(state = 0, content = "Task 5"),
-            Task(state = 0, content = "Task 6"),
-            Task(state = 0, content = "Task 7"),
-            Task(state = 0, content = "Task 8"),
-            Task(state = 0, content = "Task 9"),
-            Task(state = 0, content = "Task 10"),
-            Task(state = 0, content = "Task 11"),
-            Task(state = 0, content = "Task 12"),
-            Task(state = 0, content = "Task 13"),
-            Task(state = 0, content = "Task 14")
+                Task(state = 0, content = "Task 1"),
+                Task(state = 0, content = "Task 2"),
+                Task(state = 0, content = "Task 3"),
+                Task(state = 0, content = "Task 4"),
+                Task(state = 0, content = "Task 5"),
+                Task(state = 0, content = "Task 11"),
+                Task(state = 0, content = "Task 12"),
+                Task(state = 0, content = "Task 13"),
+                Task(state = 0, content = "Task 14")
+        )
+    }
+
+    fun getCompletedTaskList(): List<DataListItem> {
+        return listOf(
+            Task(state = 1, content = "Task 6"),
+            Task(state = 1, content = "Task 7"),
+            Task(state = 1, content = "Task 8"),
+            Task(state = 1, content = "Task 9"),
+            Task(state = 1, content = "Task 10"),
+
         )
     }
 }
